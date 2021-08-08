683e9c2ad06882d6e64f5904aa632aa3b17c9dd6
Kent Ross <k@mad.cash>
2021-08-08 15:10:30 -0700

    Implement std::pair::operator<=>
    Implements synth-three-way and three way comparison for std::pair. This patch consists code out of scope, as the relevant portions are blocked on the modularisation of the concepts library and the implementation of the std::three_way_comparable_with concept.
    
    Differential Revision: https://reviews.llvm.org/D107721
    
    actually this file is up for review
    
    work on libcxx checklist
    
    add tests
    
    Differential Revision: https://reviews.llvm.org/D107721

---
+++ libcxx/test/libcxx/library/description/conventions/expos.only.func/synth_three_way.pass.cpp
@@ -0,0 +1,104 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17, libcpp-no-concepts
+
+// template<class T, class U> auto __synth_three_way(T __t, U __u);
+
+#include <cassert>
+#include <compare>
+#include <limits>
+#include <utility> // Includes synth-three-way via std::pair::operator<=>
+
+#include "test_macros.h"
+
+// A custom three-way result type
+struct CustomResult {
+  friend constexpr bool operator==(int, const CustomResult&) noexcept { return true; }
+  friend constexpr bool operator==(const CustomResult&, int) noexcept { return true; }
+  friend constexpr bool operator<(int, const CustomResult&) noexcept { return false; }
+  friend constexpr bool operator<(const CustomResult&, int) noexcept { return false; }
+};
+
+int main(int, char**) {
+  static_assert(std::__synth_three_way(1, 2) == std::strong_ordering::less);
+  ASSERT_SAME_TYPE(std::strong_ordering, std::__synth_three_way_result<int, int>);
+
+  {
+    struct NoSpaceship {
+      int value;
+      constexpr bool operator==(const NoSpaceship&) const = default;
+      constexpr bool operator<(const NoSpaceship& other) const { return value < other.value; };
+    };
+    static_assert(NoSpaceship{1} < NoSpaceship{2}, "");
+    static_assert(!(NoSpaceship{1} < NoSpaceship{1}), "");
+    static_assert(std::__synth_three_way(NoSpaceship{1}, NoSpaceship{1}) == std::weak_ordering::equivalent);
+    static_assert(std::__synth_three_way(NoSpaceship{2}, NoSpaceship{1}) == std::weak_ordering::greater);
+    static_assert(std::__synth_three_way(NoSpaceship{1}, NoSpaceship{2}) == std::weak_ordering::less);
+    ASSERT_SAME_TYPE(std::weak_ordering, std::__synth_three_way_result<NoSpaceship, NoSpaceship>);
+  }
+  {
+    struct WithSpaceship {
+      int value;
+      constexpr bool operator==(const WithSpaceship&) const = default;
+      constexpr std::strong_ordering operator<=>(const WithSpaceship& other) const { return value <=> other.value; };
+    };
+    static_assert(WithSpaceship{1} < WithSpaceship{2}, "");
+    static_assert(!(WithSpaceship{1} < WithSpaceship{1}), "");
+    static_assert(std::__synth_three_way(WithSpaceship{1}, WithSpaceship{1}) == std::strong_ordering::equivalent, "");
+    static_assert(std::__synth_three_way(WithSpaceship{2}, WithSpaceship{1}) == std::strong_ordering::greater, "");
+    static_assert(std::__synth_three_way(WithSpaceship{1}, WithSpaceship{2}) == std::strong_ordering::less, "");
+    ASSERT_SAME_TYPE(std::strong_ordering, std::__synth_three_way_result<WithSpaceship, WithSpaceship>);
+  }
+  {
+    struct WithPartialSpaceship {
+      double value;
+      constexpr bool operator==(const WithPartialSpaceship&) const = default;
+      constexpr std::partial_ordering operator<=>(const WithPartialSpaceship& other) const {
+        return value <=> other.value;
+      }
+    };
+    static_assert(WithPartialSpaceship{1.0} < WithPartialSpaceship{2.0}, "");
+    static_assert(!(WithPartialSpaceship{1.0} < WithPartialSpaceship{1.0}), "");
+    static_assert(std::__synth_three_way(WithPartialSpaceship{1.0},
+                                         WithPartialSpaceship{1.0}) == std::partial_ordering::equivalent, "");
+    static_assert(std::__synth_three_way(WithPartialSpaceship{2.0},
+                                         WithPartialSpaceship{1.0}) == std::partial_ordering::greater, "");
+    static_assert(std::__synth_three_way(WithPartialSpaceship{1.0},
+                                         WithPartialSpaceship{2.0}) == std::partial_ordering::less, "");
+    static_assert(std::__synth_three_way(WithPartialSpaceship{std::numeric_limits<double>::quiet_NaN()},
+                                         WithPartialSpaceship{std::numeric_limits<double>::quiet_NaN()})
+                                                                    == std::partial_ordering::unordered, "");
+    ASSERT_SAME_TYPE(std::partial_ordering, std::__synth_three_way_result<WithPartialSpaceship, WithPartialSpaceship>);
+  }
+  {
+    // Types with operator<=> but no operator== are not three_way_comparable and will fall back to operator< and
+    // compare as weakly ordered.
+    struct SpaceshipNoEquals {
+      constexpr std::strong_ordering operator<=>(const SpaceshipNoEquals&) const { return std::strong_ordering::equivalent; }
+      constexpr bool operator<(const SpaceshipNoEquals&) const { return false; }
+    };
+    static_assert(std::__synth_three_way(SpaceshipNoEquals{}, SpaceshipNoEquals{}) == std::weak_ordering::equivalent, "");
+    ASSERT_SAME_TYPE(std::weak_ordering, std::__synth_three_way_result<SpaceshipNoEquals, SpaceshipNoEquals>);
+  }
+  {
+    // Custom three-way-comparison result types cannot satisfy standard concepts (and therefore synth-three-way)
+    // because they are not understood by std::common_comparison_category, but they can still be used in
+    // the same way as standard orderings to do comparisons, and thus can be used by synth-three-way to yield a
+    // weakly-ordered result.
+    struct CustomSpaceship {
+      constexpr CustomResult operator<=>(const CustomSpaceship&) const { return CustomResult(); }
+    };
+    static_assert((CustomSpaceship() <=> CustomSpaceship()) == 0, "");
+    static_assert(!(CustomSpaceship() < CustomSpaceship()), "");
+    static_assert(std::__synth_three_way(CustomSpaceship(), CustomSpaceship()) == std::weak_ordering::equivalent, "");
+    ASSERT_SAME_TYPE(std::weak_ordering, std::__synth_three_way_result<CustomSpaceship, CustomSpaceship>);
+  }
+
+  return 0;
+}
---
+++ libcxx/test/libcxx/utilities/utility/pairs/pairs.pair/comparison.pass.cpp
@@ -0,0 +1,53 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17, libcpp-no-concepts
+
+#include <cassert>
+#include <limits>
+#include <type_traits>
+#include <utility>
+
+#include "test_macros.h"
+
+int main(int, char**) {
+  {
+    // Pairs of types that both have strong ordering should compare with strong ordering.
+    static_assert((std::make_pair(1, 1) <=> std::make_pair(1, 2)) < 0, "");
+    static_assert((std::make_pair(2, 1) <=> std::make_pair(1, 2)) > 0, "");
+    auto same = std::make_pair(0, 0) <=> std::make_pair(0, 0);
+    assert(same == 0);
+    ASSERT_SAME_TYPE(std::strong_ordering, decltype(same));
+  }
+  {
+    // Pairs of int and a type with no spaceship operator should compare with weak ordering.
+    struct NoSpaceship {
+      int value;
+      constexpr bool operator==(const NoSpaceship&) const = default;
+      constexpr bool operator<(const NoSpaceship& other) const { return value < other.value; }
+    };
+    static_assert((std::make_pair(1, NoSpaceship{1}) <=> std::make_pair(1, NoSpaceship{2})) < 0, "");
+    static_assert((std::make_pair(2, NoSpaceship{1}) <=> std::make_pair(1, NoSpaceship{2})) > 0, "");
+    auto same = std::make_pair(0, NoSpaceship{0}) <=> std::make_pair(0, NoSpaceship{0});
+    assert(same == 0);
+    ASSERT_SAME_TYPE(std::weak_ordering, decltype(same));
+  }
+  {
+    // Pairs of int (strongly ordered) and double (partially ordered) should three-way compare with partial ordering.
+    static_assert((std::make_pair(1, 1.0) <=> std::make_pair(1, 2.0)) < 0, "");
+    static_assert((std::make_pair(2, 1.0) <=> std::make_pair(1, 1.0)) > 0, "");
+    static_assert((std::make_pair(std::numeric_limits<double>::quiet_NaN(), 1)
+                   <=> std::make_pair(std::numeric_limits<double>::quiet_NaN(), 2))
+                  == std::partial_ordering::unordered, "");
+    auto same = std::make_pair(0, 0.0) <=> std::make_pair(0, 0.0);
+    assert(same == 0);
+    ASSERT_SAME_TYPE(std::partial_ordering, decltype(same));
+  }
+
+  return 0;
+}

875e4d863aaf4dbe13c3af259f1424b32b446e98
Kent Ross <k@mad.cash>
2021-08-09 17:34:12 -0700

    Implement std::tuple::operator<=>
    Implement three-way comparison for tuples and expand testing. This diff is awaiting some three-way comparison concepts and synth-three-way, which are to be provided in other diffs and are not for review here.
    
    Differential Revision: https://reviews.llvm.org/D108250

---
+++ libcxx/test/std/utilities/tuple/tuple.tuple/tuple.rel/lt_incompatible.fail.cpp
@@ -0,0 +1,40 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// <tuple>
+
+// template <class... Types> class tuple;
+
+// template<class... TTypes, class... UTypes>
+//   bool
+//   operator<(const tuple<TTypes...>& t, const tuple<UTypes...>& u);
+
+// UNSUPPORTED: c++03
+
+#include <tuple>
+
+#include "test_macros.h"
+
+int main(int, char**)
+{
+  static_cast<void>(std::tuple<int>(1) < std::tuple<int, long>(1, 2)); // expected-note {{requested here}}
+  static_cast<void>(std::tuple<int, long>(1, 2) < std::tuple<int>(1)); // expected-note {{requested here}}
+  // Always
+  // expected-error-re@tuple:* 2 {{static_assert failed{{.*}} "Can't compare tuples of different sizes"}}
+
+  // c++11, c++14, c++17
+  // expected-error-re@__tuple:* 0-1 {{static_assert failed{{.*}} "tuple_element index out of range"}}
+  // expected-error@tuple:* 0-1 {{no viable conversion from}}
+
+  // c++14, c++17
+  // expected-error@tuple:* 0-1 {{no matching function for call to 'get'}}
+
+  // c++20 onwards
+  // expected-error@tuple:* 0-2 {{pack expansion contains parameter packs '_Tp' and '_Up' that have different lengths}}
+  return 0;
+}
---
+++ libcxx/test/std/utilities/tuple/tuple.tuple/tuple.rel/three_way.pass.cpp
@@ -0,0 +1,178 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// <tuple>
+
+// template <class... Types> class tuple;
+
+// template<class... TTypes, class... UTypes>
+//   auto
+//   operator<=>(const tuple<TTypes...>& t, const tuple<UTypes...>& u);
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17, libcpp-no-concepts
+
+#include <compare>
+#include <limits>
+#include <tuple>
+
+#include "test_macros.h"
+
+// A custom three-way result type
+struct CustomResult {
+  friend constexpr bool operator==(int, const CustomResult&) noexcept { return true; }
+  friend constexpr bool operator==(const CustomResult&, int) noexcept { return true; }
+  friend constexpr bool operator<(int, const CustomResult&) noexcept { return false; }
+  friend constexpr bool operator<(const CustomResult&, int) noexcept { return false; }
+};
+
+int main(int, char**)
+{
+    // Empty tuple
+    {
+        typedef std::tuple<> T0;
+        static_assert((T0() <=> T0()) == std::strong_ordering::equal, "");
+        // No member types yields strong ordering (all are equal).
+        ASSERT_SAME_TYPE(decltype(T0() <=> T0()), std::strong_ordering);
+    }
+    // Mixed types with integers, which compare strongly ordered
+    {
+        typedef std::tuple<long> T1;
+        typedef std::tuple<short> T2;
+        ASSERT_SAME_TYPE(decltype(T1() <=> T2()), std::strong_ordering);
+        static_assert((T1(1) <=> T2(1)) == std::strong_ordering::equal, "");
+        static_assert((T1(1) <=> T2(0)) == std::strong_ordering::greater, "");
+        static_assert((T1(1) <=> T2(2)) == std::strong_ordering::less, "");
+    }
+    {
+        typedef std::tuple<long, int> T1;
+        typedef std::tuple<short, long> T2;
+        ASSERT_SAME_TYPE(decltype(T1() <=> T2()), std::strong_ordering);
+        static_assert((T1(1, 2) <=> T2(1, 2)) == std::strong_ordering::equal, "");
+        static_assert((T1(1, 2) <=> T2(0, 2)) == std::strong_ordering::greater, "");
+        static_assert((T1(1, 2) <=> T2(2, 2)) == std::strong_ordering::less, "");
+        static_assert((T1(1, 2) <=> T2(1, 1)) == std::strong_ordering::greater, "");
+        static_assert((T1(1, 2) <=> T2(1, 3)) == std::strong_ordering::less, "");
+    }
+    {
+        typedef std::tuple<long, int, short> T1;
+        typedef std::tuple<short, long, int> T2;
+        ASSERT_SAME_TYPE(decltype(T1() <=> T2()), std::strong_ordering);
+        static_assert((T1(1, 2, 3) <=> T2(1, 2, 3)) == std::strong_ordering::equal, "");
+        static_assert((T1(1, 2, 3) <=> T2(0, 2, 3)) == std::strong_ordering::greater, "");
+        static_assert((T1(1, 2, 3) <=> T2(2, 2, 3)) == std::strong_ordering::less, "");
+        static_assert((T1(1, 2, 3) <=> T2(1, 1, 3)) == std::strong_ordering::greater, "");
+        static_assert((T1(1, 2, 3) <=> T2(1, 3, 3)) == std::strong_ordering::less, "");
+        static_assert((T1(1, 2, 3) <=> T2(1, 2, 2)) == std::strong_ordering::greater, "");
+        static_assert((T1(1, 2, 3) <=> T2(1, 2, 4)) == std::strong_ordering::less, "");
+    }
+    // Mixed types with floating point, which compare partially ordered
+    {
+        typedef std::tuple<long> T1;
+        typedef std::tuple<double> T2;
+        ASSERT_SAME_TYPE(decltype(T1() <=> T2()), std::partial_ordering);
+        static_assert((T1(1) <=> T2(1)) == std::partial_ordering::equivalent, "");
+        static_assert((T1(1) <=> T2(0.9)) == std::partial_ordering::greater, "");
+        static_assert((T1(1) <=> T2(1.1)) == std::partial_ordering::less, "");
+        static_assert((T1(1) <=> T2(std::numeric_limits<double>::quiet_NaN()))
+                      == std::partial_ordering::unordered, "");
+    }
+    {
+        typedef std::tuple<long, float> T1;
+        typedef std::tuple<double, int> T2;
+        ASSERT_SAME_TYPE(decltype(T1() <=> T2()), std::partial_ordering);
+        static_assert((T1(1, 2) <=> T2(1, 2)) == std::partial_ordering::equivalent, "");
+        static_assert((T1(1, 2) <=> T2(0.9, 2)) == std::partial_ordering::greater, "");
+        static_assert((T1(1, 2) <=> T2(1.1, 2)) == std::partial_ordering::less, "");
+        static_assert((T1(1, 2) <=> T2(1, 1)) == std::partial_ordering::greater, "");
+        static_assert((T1(1, 2) <=> T2(1, 3)) == std::partial_ordering::less, "");
+        static_assert((T1(1, 2) <=> T2(std::numeric_limits<double>::quiet_NaN(), 2))
+                      == std::partial_ordering::unordered, "");
+        static_assert((T1(1, std::numeric_limits<float>::quiet_NaN()) <=> T2(1, 2))
+                      == std::partial_ordering::unordered, "");
+    }
+    {
+        typedef std::tuple<short, float, double> T1;
+        typedef std::tuple<double, long, int> T2;
+        ASSERT_SAME_TYPE(decltype(T1() <=> T2()), std::partial_ordering);
+        static_assert((T1(1, 2, 3) <=> T2(1, 2, 3)) == std::partial_ordering::equivalent, "");
+        static_assert((T1(1, 2, 3) <=> T2(0.9, 2, 3)) == std::partial_ordering::greater, "");
+        static_assert((T1(1, 2, 3) <=> T2(1.1, 2, 3)) == std::partial_ordering::less, "");
+        static_assert((T1(1, 2, 3) <=> T2(1, 1, 3)) == std::partial_ordering::greater, "");
+        static_assert((T1(1, 2, 3) <=> T2(1, 3, 3)) == std::partial_ordering::less, "");
+        static_assert((T1(1, 2, 3) <=> T2(1, 2, 2)) == std::partial_ordering::greater, "");
+        static_assert((T1(1, 2, 3) <=> T2(1, 2, 4)) == std::partial_ordering::less, "");
+        static_assert((T1(1, 2, 3) <=> T2(std::numeric_limits<double>::quiet_NaN(), 2, 3))
+                      == std::partial_ordering::unordered, "");
+        static_assert((T1(1, std::numeric_limits<float>::quiet_NaN(), 3) <=> T2(1, 2, 3))
+                      == std::partial_ordering::unordered, "");
+        static_assert((T1(1, 2, std::numeric_limits<double>::quiet_NaN()) <=> T2(1, 2, 3))
+                      == std::partial_ordering::unordered, "");
+    }
+    // Ordering classes and synthesized three way comparison
+    {
+        typedef std::tuple<long, int, int> T1;
+        typedef std::tuple<int, long, int> T2;
+        // All strongly ordered members yields strong ordering.
+        ASSERT_SAME_TYPE(decltype(T1() <=> T2()), std::strong_ordering);
+    }
+    {
+        struct WeakSpaceship {
+            constexpr bool operator==(const WeakSpaceship&) const { return true; }
+            constexpr std::weak_ordering operator<=>(const WeakSpaceship&) const {
+                return std::weak_ordering::equivalent;
+            }
+        };
+        {
+            typedef std::tuple<int, int, WeakSpaceship> T1;
+            typedef std::tuple<int, long, WeakSpaceship> T2;
+            // Strongly ordered members and a weakly ordered member yields weak ordering.
+            ASSERT_SAME_TYPE(decltype(T1() <=> T2()), std::weak_ordering);
+        }
+        {
+            typedef std::tuple<int, int, WeakSpaceship> T1;
+            typedef std::tuple<double, long, WeakSpaceship> T2;
+            // Doubles are partially ordered, so one partial, one strong, and one weak ordering
+            // yields partial ordering.
+            ASSERT_SAME_TYPE(decltype(T1() <=> T2()), std::partial_ordering);
+        }
+    }
+    {
+        struct NoSpaceship {
+            constexpr bool operator==(const NoSpaceship&) const { return true; }
+            constexpr bool operator<(const NoSpaceship&) const { return false; }
+        };
+        typedef std::tuple<int, int, NoSpaceship> T1;
+        typedef std::tuple<int, long, NoSpaceship> T2;
+        // Strongly ordered members and a weakly ordered member (synthesized) yields weak ordering.
+        ASSERT_SAME_TYPE(decltype(T1() <=> T2()), std::weak_ordering);
+    }
+    {
+        struct SpaceshipNoEquals {
+            constexpr std::strong_ordering operator<=>(const SpaceshipNoEquals&) const {
+                return std::strong_ordering::equal;
+            }
+            constexpr bool operator<(const SpaceshipNoEquals&) const { return false; }
+        };
+        typedef std::tuple<int, int, SpaceshipNoEquals> T1;
+        typedef std::tuple<int, long, SpaceshipNoEquals> T2;
+        // Spaceship operator with no == operator falls back on the < operator and weak ordering.
+        ASSERT_SAME_TYPE(decltype(T1() <=> T2()), std::weak_ordering);
+    }
+    {
+        struct CustomSpaceship {
+            constexpr CustomResult operator<=>(const CustomSpaceship&) const { return CustomResult(); }
+        };
+        typedef std::tuple<int, int, CustomSpaceship> T1;
+        typedef std::tuple<short, long, CustomSpaceship> T2;
+        // Custom three way return types cannot be used in synthesized three way comparison,
+        // but they can be used for (rewritten) operator< when synthesizing a weak ordering.
+        ASSERT_SAME_TYPE(decltype(T1() <=> T2()), std::weak_ordering);
+    }
+
+  return 0;
+}

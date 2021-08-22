f16963eab3dea7059c3a374b04ec4336c55d18ed
Kent Ross <k@mad.cash>
2021-08-22 13:38:40 -0700

    updates from feedback

--- libcxx/test/std/utilities/tuple/tuple.tuple/tuple.rel/three_way.pass.cpp
+++ libcxx/test/std/utilities/tuple/tuple.tuple/tuple.rel/three_way.pass.cpp
@@ -25,72 +25,72 @@
 
 // A custom three-way result type
 struct CustomEquality {
-  friend constexpr bool operator==(const CustomEquality&, int) noexcept { return true; }
-  friend constexpr bool operator<(const CustomEquality&, int) noexcept { return false; }
-  friend constexpr bool operator<(int, const CustomEquality&) noexcept { return false; }
+    friend constexpr bool operator==(const CustomEquality&, int) noexcept { return true; }
+    friend constexpr bool operator<(const CustomEquality&, int) noexcept { return false; }
+    friend constexpr bool operator<(int, const CustomEquality&) noexcept { return false; }
 };
 
-int main(int, char**)
+constexpr bool test()
 {
     // Empty tuple
     {
         typedef std::tuple<> T0;
-        static_assert((T0() <=> T0()) == std::strong_ordering::equal, "");
         // No member types yields strong ordering (all are equal).
         ASSERT_SAME_TYPE(decltype(T0() <=> T0()), std::strong_ordering);
+        assert((T0() <=> T0()) == std::strong_ordering::equal);
     }
     // Mixed types with integers, which compare strongly ordered
     {
         typedef std::tuple<long> T1;
         typedef std::tuple<short> T2;
         ASSERT_SAME_TYPE(decltype(T1() <=> T2()), std::strong_ordering);
-        static_assert((T1(1) <=> T2(1)) == std::strong_ordering::equal, "");
-        static_assert((T1(1) <=> T2(0)) == std::strong_ordering::greater, "");
-        static_assert((T1(1) <=> T2(2)) == std::strong_ordering::less, "");
+        assert((T1(1) <=> T2(1)) == std::strong_ordering::equal);
+        assert((T1(1) <=> T2(0)) == std::strong_ordering::greater);
+        assert((T1(1) <=> T2(2)) == std::strong_ordering::less);
     }
     {
-        typedef std::tuple<long, int> T1;
-        typedef std::tuple<short, long> T2;
+        typedef std::tuple<long, unsigned int> T1;
+        typedef std::tuple<short, unsigned long> T2;
         ASSERT_SAME_TYPE(decltype(T1() <=> T2()), std::strong_ordering);
-        static_assert((T1(1, 2) <=> T2(1, 2)) == std::strong_ordering::equal, "");
-        static_assert((T1(1, 2) <=> T2(0, 2)) == std::strong_ordering::greater, "");
-        static_assert((T1(1, 2) <=> T2(2, 2)) == std::strong_ordering::less, "");
-        static_assert((T1(1, 2) <=> T2(1, 1)) == std::strong_ordering::greater, "");
-        static_assert((T1(1, 2) <=> T2(1, 3)) == std::strong_ordering::less, "");
+        assert((T1(1, 2) <=> T2(1, 2)) == std::strong_ordering::equal);
+        assert((T1(1, 2) <=> T2(0, 2)) == std::strong_ordering::greater);
+        assert((T1(1, 2) <=> T2(2, 2)) == std::strong_ordering::less);
+        assert((T1(1, 2) <=> T2(1, 1)) == std::strong_ordering::greater);
+        assert((T1(1, 2) <=> T2(1, 3)) == std::strong_ordering::less);
     }
     {
-        typedef std::tuple<long, int, short> T1;
-        typedef std::tuple<short, long, int> T2;
+        typedef std::tuple<long, int, unsigned short> T1;
+        typedef std::tuple<short, long, unsigned int> T2;
         ASSERT_SAME_TYPE(decltype(T1() <=> T2()), std::strong_ordering);
-        static_assert((T1(1, 2, 3) <=> T2(1, 2, 3)) == std::strong_ordering::equal, "");
-        static_assert((T1(1, 2, 3) <=> T2(0, 2, 3)) == std::strong_ordering::greater, "");
-        static_assert((T1(1, 2, 3) <=> T2(2, 2, 3)) == std::strong_ordering::less, "");
-        static_assert((T1(1, 2, 3) <=> T2(1, 1, 3)) == std::strong_ordering::greater, "");
-        static_assert((T1(1, 2, 3) <=> T2(1, 3, 3)) == std::strong_ordering::less, "");
-        static_assert((T1(1, 2, 3) <=> T2(1, 2, 2)) == std::strong_ordering::greater, "");
-        static_assert((T1(1, 2, 3) <=> T2(1, 2, 4)) == std::strong_ordering::less, "");
+        assert((T1(1, 2, 3) <=> T2(1, 2, 3)) == std::strong_ordering::equal);
+        assert((T1(1, 2, 3) <=> T2(0, 2, 3)) == std::strong_ordering::greater);
+        assert((T1(1, 2, 3) <=> T2(2, 2, 3)) == std::strong_ordering::less);
+        assert((T1(1, 2, 3) <=> T2(1, 1, 3)) == std::strong_ordering::greater);
+        assert((T1(1, 2, 3) <=> T2(1, 3, 3)) == std::strong_ordering::less);
+        assert((T1(1, 2, 3) <=> T2(1, 2, 2)) == std::strong_ordering::greater);
+        assert((T1(1, 2, 3) <=> T2(1, 2, 4)) == std::strong_ordering::less);
     }
     // Mixed types with floating point, which compare partially ordered
     {
         typedef std::tuple<long> T1;
         typedef std::tuple<double> T2;
         ASSERT_SAME_TYPE(decltype(T1() <=> T2()), std::partial_ordering);
-        static_assert((T1(1) <=> T2(1)) == std::partial_ordering::equivalent, "");
-        static_assert((T1(1) <=> T2(0.9)) == std::partial_ordering::greater, "");
-        static_assert((T1(1) <=> T2(1.1)) == std::partial_ordering::less, "");
+        assert((T1(1) <=> T2(1)) == std::partial_ordering::equivalent);
+        assert((T1(1) <=> T2(0.9)) == std::partial_ordering::greater);
+        assert((T1(1) <=> T2(1.1)) == std::partial_ordering::less);
         // Comparisons with NaN are non-constexpr in GCC
         assert((T1(1) <=> T2(std::numeric_limits<double>::quiet_NaN()))
                       == std::partial_ordering::unordered);
     }
     {
         typedef std::tuple<long, float> T1;
-        typedef std::tuple<double, int> T2;
+        typedef std::tuple<double, unsigned int> T2;
         ASSERT_SAME_TYPE(decltype(T1() <=> T2()), std::partial_ordering);
-        static_assert((T1(1, 2) <=> T2(1, 2)) == std::partial_ordering::equivalent, "");
-        static_assert((T1(1, 2) <=> T2(0.9, 2)) == std::partial_ordering::greater, "");
-        static_assert((T1(1, 2) <=> T2(1.1, 2)) == std::partial_ordering::less, "");
-        static_assert((T1(1, 2) <=> T2(1, 1)) == std::partial_ordering::greater, "");
-        static_assert((T1(1, 2) <=> T2(1, 3)) == std::partial_ordering::less, "");
+        assert((T1(1, 2) <=> T2(1, 2)) == std::partial_ordering::equivalent);
+        assert((T1(1, 2) <=> T2(0.9, 2)) == std::partial_ordering::greater);
+        assert((T1(1, 2) <=> T2(1.1, 2)) == std::partial_ordering::less);
+        assert((T1(1, 2) <=> T2(1, 1)) == std::partial_ordering::greater);
+        assert((T1(1, 2) <=> T2(1, 3)) == std::partial_ordering::less);
         // Comparisons with NaN are non-constexpr in GCC
         assert((T1(1, 2) <=> T2(std::numeric_limits<double>::quiet_NaN(), 2))
                       == std::partial_ordering::unordered);
@@ -99,15 +99,15 @@ int main(int, char**)
     }
     {
         typedef std::tuple<short, float, double> T1;
-        typedef std::tuple<double, long, int> T2;
+        typedef std::tuple<double, long, unsigned int> T2;
         ASSERT_SAME_TYPE(decltype(T1() <=> T2()), std::partial_ordering);
-        static_assert((T1(1, 2, 3) <=> T2(1, 2, 3)) == std::partial_ordering::equivalent, "");
-        static_assert((T1(1, 2, 3) <=> T2(0.9, 2, 3)) == std::partial_ordering::greater, "");
-        static_assert((T1(1, 2, 3) <=> T2(1.1, 2, 3)) == std::partial_ordering::less, "");
-        static_assert((T1(1, 2, 3) <=> T2(1, 1, 3)) == std::partial_ordering::greater, "");
-        static_assert((T1(1, 2, 3) <=> T2(1, 3, 3)) == std::partial_ordering::less, "");
-        static_assert((T1(1, 2, 3) <=> T2(1, 2, 2)) == std::partial_ordering::greater, "");
-        static_assert((T1(1, 2, 3) <=> T2(1, 2, 4)) == std::partial_ordering::less, "");
+        assert((T1(1, 2, 3) <=> T2(1, 2, 3)) == std::partial_ordering::equivalent);
+        assert((T1(1, 2, 3) <=> T2(0.9, 2, 3)) == std::partial_ordering::greater);
+        assert((T1(1, 2, 3) <=> T2(1.1, 2, 3)) == std::partial_ordering::less);
+        assert((T1(1, 2, 3) <=> T2(1, 1, 3)) == std::partial_ordering::greater);
+        assert((T1(1, 2, 3) <=> T2(1, 3, 3)) == std::partial_ordering::less);
+        assert((T1(1, 2, 3) <=> T2(1, 2, 2)) == std::partial_ordering::greater);
+        assert((T1(1, 2, 3) <=> T2(1, 2, 4)) == std::partial_ordering::less);
         // Comparisons with NaN are non-constexpr in GCC
         assert((T1(1, 2, 3) <=> T2(std::numeric_limits<double>::quiet_NaN(), 2, 3))
                       == std::partial_ordering::unordered);
@@ -118,8 +118,8 @@ int main(int, char**)
     }
     // Ordering classes and synthesized three way comparison
     {
-        typedef std::tuple<long, int, int> T1;
-        typedef std::tuple<int, long, int> T2;
+        typedef std::tuple<long, int, unsigned int> T1;
+        typedef std::tuple<int, long, unsigned short> T2;
         // All strongly ordered members yields strong ordering.
         ASSERT_SAME_TYPE(decltype(T1() <=> T2()), std::strong_ordering);
     }
@@ -131,13 +131,13 @@ int main(int, char**)
             }
         };
         {
-            typedef std::tuple<int, int, WeakSpaceship> T1;
-            typedef std::tuple<int, long, WeakSpaceship> T2;
+            typedef std::tuple<int, unsigned int, WeakSpaceship> T1;
+            typedef std::tuple<int, unsigned long, WeakSpaceship> T2;
             // Strongly ordered members and a weakly ordered member yields weak ordering.
             ASSERT_SAME_TYPE(decltype(T1() <=> T2()), std::weak_ordering);
         }
         {
-            typedef std::tuple<int, int, WeakSpaceship> T1;
+            typedef std::tuple<unsigned int, int, WeakSpaceship> T1;
             typedef std::tuple<double, long, WeakSpaceship> T2;
             // Doubles are partially ordered, so one partial, one strong, and one weak ordering
             // yields partial ordering.
@@ -149,8 +149,8 @@ int main(int, char**)
             constexpr bool operator==(const NoSpaceship&) const { return true; }
             constexpr bool operator<(const NoSpaceship&) const { return false; }
         };
-        typedef std::tuple<int, int, NoSpaceship> T1;
-        typedef std::tuple<int, long, NoSpaceship> T2;
+        typedef std::tuple<int, unsigned int, NoSpaceship> T1;
+        typedef std::tuple<int, unsigned long, NoSpaceship> T2;
         // Strongly ordered members and a weakly ordered member (synthesized) yields weak ordering.
         ASSERT_SAME_TYPE(decltype(T1() <=> T2()), std::weak_ordering);
     }
@@ -161,8 +161,8 @@ int main(int, char**)
             }
             constexpr bool operator<(const SpaceshipNoEquals&) const { return false; }
         };
-        typedef std::tuple<int, int, SpaceshipNoEquals> T1;
-        typedef std::tuple<int, long, SpaceshipNoEquals> T2;
+        typedef std::tuple<int, unsigned int, SpaceshipNoEquals> T1;
+        typedef std::tuple<int, unsigned long, SpaceshipNoEquals> T2;
         // Spaceship operator with no == operator falls back on the < operator and weak ordering.
         ASSERT_SAME_TYPE(decltype(T1() <=> T2()), std::weak_ordering);
     }
@@ -170,12 +170,19 @@ int main(int, char**)
         struct CustomSpaceship {
             constexpr CustomEquality operator<=>(const CustomSpaceship&) const { return CustomEquality(); }
         };
-        typedef std::tuple<int, int, CustomSpaceship> T1;
-        typedef std::tuple<short, long, CustomSpaceship> T2;
+        typedef std::tuple<int, unsigned int, CustomSpaceship> T1;
+        typedef std::tuple<short, unsigned long, CustomSpaceship> T2;
         // Custom three way return types cannot be used in synthesized three way comparison,
         // but they can be used for (rewritten) operator< when synthesizing a weak ordering.
         ASSERT_SAME_TYPE(decltype(T1() <=> T2()), std::weak_ordering);
     }
 
+    return true;
+}
+
+int main(int, char**)
+{
+    static_assert(test());
+    assert(test());
     return 0;
 }

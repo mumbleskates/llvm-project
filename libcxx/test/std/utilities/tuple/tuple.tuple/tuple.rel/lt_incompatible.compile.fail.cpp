e1f1807527f2eb15d92c4e510e0fe3e54bf1ac97
Kent Ross <k@mad.cash>
2021-09-22 14:40:37 -0700

    don't cast-to-void at top-level scope

--- libcxx/test/std/utilities/tuple/tuple.tuple/tuple.rel/lt_incompatible.compile.fail.cpp
+++ libcxx/test/std/utilities/tuple/tuple.tuple/tuple.rel/lt_incompatible.compile.fail.cpp
@@ -18,17 +18,19 @@
 
 #include <tuple>
 
-static_cast<void>(std::tuple<int>(1) < std::tuple<int, long>(1, 2));
-// c++20 onwards
-// expected-error@-2 0-1 {{invalid operands to binary expression}}
-static_cast<void>(std::tuple<int, long>(1, 2) < std::tuple<int>(1));
-// c++20 onwards
-// expected-error@-2 0-1 {{invalid operands to binary expression}}
+void test() {
+  static_cast<void>(std::tuple<int>(1) < std::tuple<int, long>(1, 2));
+  // c++20 onwards
+  // expected-error@-2 0-1 {{invalid operands to binary expression}}
+  static_cast<void>(std::tuple<int, long>(1, 2) < std::tuple<int>(1));
+  // c++20 onwards
+  // expected-error@-2 0-1 {{invalid operands to binary expression}}
 
-// c++11, c++14, c++17
-// expected-error-re@tuple:* 0-2 {{static_assert failed{{.*}} "Can't compare tuples of different sizes"}}
-// expected-error-re@__tuple:* 0-1 {{static_assert failed{{.*}} "tuple_element index out of range"}}
-// expected-error@tuple:* 0-1 {{no viable conversion from}}
+  // c++11, c++14, c++17
+  // expected-error-re@tuple:* 0-2 {{static_assert failed{{.*}} "Can't compare tuples of different sizes"}}
+  // expected-error-re@__tuple:* 0-1 {{static_assert failed{{.*}} "tuple_element index out of range"}}
+  // expected-error@tuple:* 0-1 {{no viable conversion from}}
 
-// c++14, c++17
-// expected-error@tuple:* 0-1 {{no matching function for call to 'get'}}
+  // c++14, c++17
+  // expected-error@tuple:* 0-1 {{no matching function for call to 'get'}}
+}
--- libcxx/test/std/utilities/tuple/tuple.tuple/tuple.rel/ne_incompatible.compile.fail.cpp
+++ libcxx/test/std/utilities/tuple/tuple.tuple/tuple.rel/ne_incompatible.compile.fail.cpp
@@ -18,10 +18,12 @@
 
 #include <tuple>
 
-static_cast<void>(std::tuple<int>(1) != std::tuple<int, long>(1, 2)); // expected-note {{requested here}}
-// expected-error-re@tuple:* {{static_assert failed{{.*}} "Can't compare tuples of different sizes"}}
+void test() {
+  static_cast<void>(std::tuple<int>(1) != std::tuple<int, long>(1, 2)); // expected-note {{requested here}}
+  // expected-error-re@tuple:* {{static_assert failed{{.*}} "Can't compare tuples of different sizes"}}
 
-static_cast<void>(std::tuple<int, long>(1, 2) != std::tuple<int>(1)); // expected-note {{requested here}}
-// expected-error-re@tuple:* {{static_assert failed{{.*}} "Can't compare tuples of different sizes"}}
-// expected-error-re@__tuple:* {{static_assert failed{{.*}} "tuple_element index out of range"}}
-// expected-error@tuple:* {{no viable conversion from}}
+  static_cast<void>(std::tuple<int, long>(1, 2) != std::tuple<int>(1)); // expected-note {{requested here}}
+  // expected-error-re@tuple:* {{static_assert failed{{.*}} "Can't compare tuples of different sizes"}}
+  // expected-error-re@__tuple:* {{static_assert failed{{.*}} "tuple_element index out of range"}}
+  // expected-error@tuple:* {{no viable conversion from}}
+}
